9e817c650d898e8f9d1baad1e2f091693d79c647
pbeierle22 <pbeierle22@gmail.com>
2017-03-25 14:22:47 -0500

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 /**
  * Created by Addison on 3/24/2017.
+   Mostly built by Peter on 3/25/2017
+        -functionality changed slightly
+          -no longer worried about cost/requests
+          -should be just about when the room is reserved and what room is reserved
+          -that way, the hotel can update what rooms are taken by checking the reservation list
  */
 public class Reservation {
     private Guest reservee;
