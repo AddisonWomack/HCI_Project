947128036a9625815afe24d1a63f91212a278c49
pbeierle22 <pbeierle22@gmail.com>
2017-03-25 14:20:27 -0500

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -7,6 +7,45 @@ import java.util.ArrayList;
  */
 public class Reservation {
     private Guest reservee;
-    private RoomLayout requestedRoomLayout;
-    private ArrayList<Employee> completedBy;
+    private Date start;
+    private Date end;
+    private Room reserved;
+    private Employee completedBy;
+    
+    public Reservation(Date s, Date e, Room r, Guest g, Employee e) {
+        start = s;
+        end = e;
+        reservee = g;
+        completedBy = e;
+        reserved = r;
+    }
+    
+    public Date getStart() {
+        return start;
+    }
+    
+    public Date getEnd() {
+        return end;
+    }
+    
+    public Room getRoom() {
+        return reserved;
+    }
+    
+    public Guest getGuest() {
+        return reservee;
+    }
+    
+    public Employee getEmployee() {
+        return completedBy;
+    }
+    
+    public void changeDates(Date s, Date e) {
+        start = s;
+        end = e;
+    }
+    
+    public void changeRoom(Room r) {
+        reserved = r;
+    }
 }
